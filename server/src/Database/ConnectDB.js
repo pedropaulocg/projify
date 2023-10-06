10732704d7e20f88b65a08d67996581c2ff95807
pedropaulocg <pedropaulocostaguima@hotmail.com>
2023-10-06 23:11:33 +0200

    atlas connection

--- server/src/Database/ConnectDB.js
+++ server/src/Database/ConnectDB.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
+import 'dotenv/config'
 
 
 export default async function connectDB () {
   try {
-    await mongoose.connect('mongodb://127.0.0.1/projifyDB')
+    await mongoose.connect(`mongodb+srv://super:${process.env.DB_PASS}@projify2.ylpzsws.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp`)
     console.log('Connected to the DB')
   } catch ( error ) {
     console.log(error);
